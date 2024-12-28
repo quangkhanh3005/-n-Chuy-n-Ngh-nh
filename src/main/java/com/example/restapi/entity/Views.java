2d2ab786d7ce8cef31deddbb380619a6f7ccca90
quang khanh <nidakhanh0@gmail.com>
2024-12-28 16:33:49 +0700

    Remove all files from GitHub

--- src/main/java/com/example/restapi/entity/Views.java
+++
@@ -1,6 +0,0 @@
-package com.example.restapi.entity;
-
-public class Views {
-    public static class Basic {}       // Chế độ cơ bản
-    public static class Detailed extends Basic {}
-}
--- src/main/java/com/example/restapi/repository/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapi.repository;
-import com.example.restapi.entity.caterogy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriesRepository extends JpaRepository<caterogy,Integer> {
-}
